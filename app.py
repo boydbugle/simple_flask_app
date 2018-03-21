b39abd8a727a547405ac14fb39aee9c143d27c83
boyd ndonga <boydbugle@gmail.com>
2018-03-21 15:15:11 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from data import articles_dstore
+# from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+# from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 article = articles_dstore()
 
 
-@app.route('/')
+@app.route('/index')
 @app.route('/index/<path:name>')
 def index(name=None):
     """Complete this docstring"""
@@ -15,13 +17,18 @@ def index(name=None):
 @app.route('/articles')
 def articles():
     """Complete this docstring"""
-    return render_template('articles.html', articles=article)
+    return render_template('articles.html', article=article)
 
 
-@app.route('/article/<string:id>')
+@app.route('/article/<string:article_id>')
 def article(article_id):
     """Complete this docstring"""
-    return render_template('article.html', id=article_id)
+    return render_template('article.html', article_id=article_id)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
