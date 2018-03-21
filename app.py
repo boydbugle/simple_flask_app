8afe882d3934504bc650a650e9432e2ee157535b
boyd ndonga <boydbugle@gmail.com>
2018-03-21 18:44:00 +0300

    create db connection to  registartion form

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from wtforms.validators import DataRequired
 # from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
+import os
 
 app = Flask(__name__)
 
 # config mysql
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ESTD1759'
-app.config['MYSQL_DB'] = 'myflask'
-app.config['MYSQL_CURSORCLASS'] = 'Dictcursor'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ESTD1759'
+app.config['MYSQL_DATABASE_DB'] = 'myflask'
+app.config['MYSQL_DATABASE_CURSORCLASS'] = 'Dictcursor'
+
+# init mysql
+
+mysql = MySQL(app)
 
 
 article = articles_dstore()
@@ -53,9 +58,24 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        pass
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # create cursor
+        cur = mysql.get_db().cursor()
+        # execute query
+        cur.execute("INSERT INTO users(username, email, password) VALUES (%s, %s, %s)",
+                    (username, email, password))
+        mysql.get_db().commit()
+        # close cursor
+        cur.close()
+
+        flash('Bravo,  You are a registered user')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
