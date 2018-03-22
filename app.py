cbdbb4277ec940bd8e6c8784e213c689de506659
boyd ndonga <boydbugle@gmail.com>
2018-03-22 13:03:18 +0300

    create article registration route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from functools import wraps
 from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
-from data import articles_dstore
+# from data import articles_dstore
 from flaskext.mysql import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from wtforms.validators import DataRequired
@@ -19,26 +20,35 @@ app.config['MYSQL_DATABASE_DB'] = 'myflask'
 mysql = MySQL(cursorclass=DictCursor)
 mysql.init_app(app)
 
-article = articles_dstore()
+# article = articles_dstore()
 
 
-@app.route('/index')
-@app.route('/index/<path:name>')
-def index(name=None):
+@app.route('/')
+def index():
     """Complete this docstring"""
-    return render_template('dynamic.html', name=name)
+    return render_template('dynamic.html')
 
 
 @app.route('/articles')
 def articles():
-    """Complete this docstring"""
-    return render_template('articles.html', article=article)
+    cur = mysql.get_db().cursor()
+    result = cur.execute("SELECT * FROM articles")
+    if result > 0:
+        articles = cur.fetchall()
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'No Articles Present'
+        return render_template('articles.html', msg=msg)
+    cur.close()
 
 
-@app.route('/article/<string:article_id>')
-def article(article_id):
-    """Complete this docstring"""
-    return render_template('article.html', article_id=article_id)
+@app.route('/article/<string:id>')
+def article(id):
+    cur = mysql.get_db().cursor()
+    result = cur.execute("SELECT * FROM articles WHERE id= %s", (id))
+    if result > 0:
+        article = cur.fetchone()
+        return render_template('article.html', article=article)
 
 
 class RegisterForm(Form):
@@ -88,7 +98,6 @@ def login():
         if result > 0:
             # get stored hash
             data = cur.fetchone()
-            print (data)
             password = data['password']
             # compare password
             if sha256_crypt.verify(passwordLogin, password):
@@ -107,9 +116,63 @@ def login():
     return render_template('login.html')
 
 
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unautorized, please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are logged out', 'success')
+    return redirect(url_for('login'))
+
+
 @app.route('/dashboard')
+@is_logged_in
 def dashboard():
-    return render_template('dashboard.html')
+    cur = mysql.get_db().cursor()
+    result = cur.execute("SELECT * FROM articles")
+    if result > 0:
+        articles = cur.fetchall()
+        return render_template('dashboard.html', articles=articles)
+    else:
+        msg = 'No Articles Present'
+        return render_template('dashboard.html', msg=msg)
+    cur.close()
+
+
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.Length(min=30)])
+
+
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+        # create cursor
+        cur = mysql.get_db().cursor()
+        # execute
+        cur.execute("INSERT INTO articles(title, author, body) VALUES(%s, %s, %s)",
+                    (title, session['username'], body))
+        # commit
+        mysql.get_db().commit()
+        cur.close()
+
+        flash('Article Created', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('add_article.html', form=form)
 
 
 if __name__ == '__main__':
